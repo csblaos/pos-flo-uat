3f97a435d7966429fd630c5599853c9d3c22de80
iamlex01 <alexcsldev@gmail.com>
2026-01-15 10:39:58 +0700

    add request loging

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,44 +30,8 @@ module.exports = {
 		"@typescript-eslint/no-unsafe-call": "off",
 		"@typescript-eslint/no-unsafe-member-access": "off",
 		"@typescript-eslint/no-unsafe-return": "off",
-		"import/order": [
-			"warn",
-			{
-				alphabetize: {
-					caseInsensitive: true,
-					order: "asc"
-				},
-				groups: ["index", "parent", "external", "internal", "sibling", "builtin"],
-				pathGroups: [
-					{ pattern: "express**", group: "index", position: "before" },
-					{ pattern: "@configs/**", group: "index", position: "after" },
-					{ pattern: "@routers/**", group: "parent", position: "after" },
-					{ pattern: "@models/**", group: "parent", position: "after" },
-					{ pattern: "@services/**", group: "parent", position: "after" },
-					{ pattern: "@providers/**", group: "external", position: "after" },
-					{ pattern: "@middlewares/**", group: "internal", position: "after" },
-					{ pattern: "@components/**", group: "internal", position: "after" },
-					{ pattern: "@controllers/**", group: "internal", position: "after" },
-					{ pattern: "@interfaces/**", group: "internal", position: "after" },
-					{ pattern: "@validator/**", group: "internal", position: "after" },
-					{ pattern: "@utils/**", group: "internal", position: "after" },
-					{ pattern: "@tstypes/**", group: "internal", position: "after" },
-					{ pattern: "./**", group: "sibling", position: "after" }
-				],
-				pathGroupsExcludedImportTypes: ["builtin"],
-				"newlines-between": "always"
-			}
-		],
-		"sort-imports": [
-			"warn",
-			{
-				allowSeparatedGroups: true,
-				ignoreCase: true,
-				ignoreDeclarationSort: true,
-				ignoreMemberSort: true,
-				memberSyntaxSortOrder: ["none", "all", "multiple", "single"]
-			}
-		],
+    "import/order": "off",
+    "sort-imports": "off",
 		"node/no-missing-import": 0,
 		"node/no-unpublished-import": 0,
 		"unused-imports/no-unused-imports": "error",
@@ -78,11 +42,11 @@ module.exports = {
 		"import/no-unresolved": [2, { ignore: ["^@.*"] }],
 		"no-multiple-empty-lines": ["warn", { max: 1, maxEOF: 0 }],
 		eqeqeq: 1,
-		quotes: [2, "double"],
-		indent: [2, "tab"],
-		semi: [1, "always"],
+    quotes: [2, "double"],
+    indent: "off",
+    semi: "off",
 		camelcase: 0,
-		"comma-dangle": ["warn", "never"],
+    "comma-dangle": "off",
 		"no-tabs": [2, { allowIndentationTabs: true }],
 		"line-comment-position": 0,
 		"array-callback-return": 1,
